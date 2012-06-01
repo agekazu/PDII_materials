884b341f3b0758a7cf9a5cb681745ad0009d478a
rice-american <ie.e115703@gmail.com>
2012-06-01 14:50:32 +0900

    ボタンの座標がずれるのを防いだ

--- SceneCanvas/SceneCanvas.js
+++ SceneCanvas/SceneCanvas.js
@@ -1,5 +1,9 @@
 //明示的にグローバル変数であることを示している
 var __game__;
+var __startTime__;
+var __count__ = 0;
+var __fps__ = 30;
+
 
 //全ての画面が継承するメソッドScene
 function Scene(game,context,name){
@@ -113,11 +117,20 @@ function Game(canvas,width,height){
   //各シーンが格納される連想配列の宣言
   this.gameScenes = new Array();
   this.timerId;
+  this.offsetLeft = canvas.offsetLeft;
+  this.offsetTop = canvas.offsetTop;
+  this.mouseX;
+  this.mouseY;
 }
 
 /******************************************************
   Gameの持つprototype
  ******************************************************/
+
+Game.prototype.setFPS = function(fps){
+    __fps__ = fps;
+}
+
 
 Game.prototype.init = function(){
 }
@@ -125,12 +138,24 @@ Game.prototype.init = function(){
 Game.prototype.start = function(){
   this.init();
   __game__ = this;
-  this.timerId = setInterval(function(){
-    __game__.scene.loop();
-  },1000/30);
+  __startTime__ = new Date();
+  this.loop();
   onclick = function(e){
+    __game__.mouseX = e.pageX-__game__.offsetLeft;
+    __game__.mouseY = e.pageY-__game__.offsetTop;
     __game__.scene.onclick(e);
   }
+}
+
+Game.prototype.loop = function loop(){
+	var afterTime = ((new Date()).getTime())%100;
+	var beforeTime = (__startTime__.getTime()+(__count__*(1000/__fps__)))%100;
+	setTimeout(function(){
+		loop();
+	},(1000/__fps__)-(afterTime-beforeTime));
+	__game__.scene.loop();
+	//console.log(afterTime-beforeTime);
+	__count__++;
 }
 
 Game.prototype.addScene= function(scene){
@@ -143,6 +168,7 @@ Game.prototype.changeScene = function(sceneName){
 
 //画面の切り替えを行う
 Game.prototype.setScene = function(scene){
+  console.log(scene);
   this.scene = scene;
   this.scene.init();
   console.log("Set scene and init "+this.scene.name);
