69b45a9e671d9e01b5ce8e4227f74d0f07621dc0
rice-american <ie.e115703@gmail.com>
2012-06-10 20:24:14 +0900

    タイトルロゴ追加

--- SceneCanvas/TitleScene.js
+++ SceneCanvas/TitleScene.js
@@ -7,14 +7,24 @@ function TitleScene(game,context,name){
     var buttonwidth = 200;
     var buttonheight = 75;
 
+    var startTitle = new StartTitle(this,"startTitle",0,"codaTitle.png",(game.canvas.width/2)-250,0,400,400);
+
     var startButton = new StartButton(this,"startButton",1,"practice.gif",(game.canvas.width/2)-buttonwidth-30,(game.canvas.height)/2 + buttonheight/2,buttonwidth,buttonheight);
 
-    var startTitleCharacter = new StartCharacter(this,"startText","ソー打","30pt Arial",1,(game.canvas.width/2)-(100/2),50,100,20);
-
+    this.addParts(startTitle);
     this.addParts(startButton);
-    this.addParts(startTitleCharacter);
   }
 } 
+
+function StartTitle(scene,name,layer,imgName,x,y,width,height) {
+  this.__proto__ = new Parts(scene,name,layer,x,y,width,height);
+  var img = new Image();
+  this.loop = function(){
+    img.src = "./"+imgName;
+    //GUI部品の画像の描画
+    this.context.drawImage(img,this.x,this.y);
+  }
+}
 
 //GUI部品(gif)の描画
 function StartButton(scene,name,layer,imgName,x,y,width,height){
@@ -25,7 +35,7 @@ function StartButton(scene,name,layer,imgName,x,y,width,height){
     //GUI部品の画像の描画
     this.context.drawImage(img,this.x,this.y);
   }
- //clickされた座標を取得、ボタンの範囲内かを調べる
+  //clickされた座標を取得、ボタンの範囲内かを調べる
   this.onclick = function(e){
     var mouseX = this.game.mouseX;
     var mouseY = this.game.mouseY;
@@ -38,34 +48,4 @@ function StartButton(scene,name,layer,imgName,x,y,width,height){
 }
 
 
-  //ボタンの描画
-//  function StartButton(scene,name,x,y,width,height){
-//    //Partsを継承
-//    this.__proto__ = new Parts(scene,name,x,y,width,height);
-//    //loop関数を上書き
-//    this.loop = function(){
-//      //四角形の描画
-//      this.context.strokeRect(this.x,this.y,this.width,this.height);
-//    }
-//
-//    this.onclick = function(e){
-//      if(e.clientX >= this.x && e.clientX <= this.x + this.width
-//          && e.clientY >= this.y && e.clientY <= this.y+this.height){
-//            //this.game.setScene(this.game.gameScenes['playScene']);
-//            this.game.changeScene("playScene");
-//          } 
-//    }
-//  }
-
-  //文字列の描画
-  function StartCharacter(scene,name,text,font,layer,x,y,width,height){
-    this.__proto__ = new Parts(scene,name,layer,x,y,width,height);
-    this.font = font;
-    //loop関数を上書き
-    this.loop = function(){
-         this.context.font = this.font;
-      //font変更
-      this.context.fillText(text,this.x,this.y);            
-    }
-  }
 
