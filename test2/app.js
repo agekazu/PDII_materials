f2eaae13010cfc277c3b7e31646be47e74107a84
rice-american <ie.e115703@gmail.com>
2012-06-23 12:45:02 +0900

    個クライアント間で通信ができない

--- test2/app.js
+++ test2/app.js
@@ -4,8 +4,50 @@ var app = require('express').createServer()
 , mongo = require('mongodb')
 , Server = mongo.Server
 , Db = mongo.Db
-, __key__=0;
+, __key__= new String();
 
+
+//httpサーバを立てる
+app.listen(8080);    
+//ルートディレクトリにアクセスがあったら、index.htmlへ
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+//connectionイベント
+io.sockets.on('connection', function (socket){
+  console.log(socket.id+'が接続しました');
+  //Clientで発火したkey sendイベントへのコールバック
+  socket.on('key send',function(key){
+    __key__=key;
+    socket.emit('key push',key)
+    socket.broadcast.emit('key push', key);
+  console.log("KEY=>" + key + " TO=>" + socket.id);
+  });
+  socket.on('msg send',function(msg){
+    socket.emit('msg push' ,msg);
+    socket.broadcast.emit('msg push' , msg);
+  });
+  //disconnectイベント
+  socket.on('disconnect', function(){
+    console.log(socket.id + 'が切断しました。');
+  });
+});
+
+var user = io
+.of('/'+__key__)
+.on('connection',function(socket){
+  console.log(__key__+'connected');
+  user.on('msg send',function(msg){
+    user.emit('msg push',msg+'from'+__key__);
+  });
+}); 
+
+
+
+/**************
+  以下、DBコード
+ ***************/
 //DBサーバーオブジェクトを新しく作る   
 var server = new Server('sodadb-RiceAmerican-data-0.dotcloud.com', 30198, {auto_reconnect: true});
 var db = new Db('mydb', server);
@@ -23,41 +65,3 @@ db.open(function(err,db){
     });
   }
 });
-
-//httpサーバを立てる
-app.listen(8080);    
-//ルートディレクトリにアクセスがあったら、index.htmlへ
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
-
-//connectionイベント
-io.sockets.on('connection', function (socket){
-  console.log(socket.id+'が接続しました');
-  //Clientで発火したkey sendイベントへのコールバック
-  socket.on('key send',function(key){
-    socket.emit('key push',key)
-    socket.broadcast.emit('key push', key);
-  console.log("key=>" + key + "to=>" + socket.id);
-  });
-  socket.on('msg send' , function(msg){
-    socket.emit('msg push' ,msg);
-    socket.broadcast.emit('msg push' , msg);
-  });
-  //disconnectイベント
-  socket.on('disconnect', function(){
-    console.log(socket.id + 'が切断しました。');
-    console.log(key);
-  });
-});
-
-
-
-var user = io
-.of('/'+key)
-.on('connection',function(socket){
-  console.log(key+'connected');
-  socket.on('msg send',function(msg){
-    user.emit('msg push',msg+'from'+key);
-  });
-}); 
