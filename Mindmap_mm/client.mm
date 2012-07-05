9d0e890755006cc766acfba0a07dfdccac2c690e
rice-american <ie.e115703@gmail.com>
2012-07-05 18:49:27 +0900

    2012/07/04 ミーティングの時に更新

--- Mindmap_mm/client.mm
+++ Mindmap_mm/client.mm
@@ -15,7 +15,7 @@
     </p>
   </body>
 </html></richcontent>
-<node CREATED="1340783415169" ID="ID_829816537" MODIFIED="1340783508104">
+<node CREATED="1340783415169" ID="ID_829816537" MODIFIED="1341393602471">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -28,211 +28,240 @@
       &#12288;&#30011;&#20687;&#12398;&#35501;&#12415;&#36796;&#12415;
     </p>
     <p>
-      &#12288;init
+      &#12288;<b>init</b>
     </p>
   </body>
-</html></richcontent>
+</html>
+</richcontent>
 </node>
-<node CREATED="1340783510222" ID="ID_1149411173" MODIFIED="1340787165668">
+<node CREATED="1340783510222" ID="ID_1149411173" MODIFIED="1341393639947">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      init
+      <b>init</b>
     </p>
     <p>
-      &#12288;socket&#12458;&#12502;&#12472;&#12455;&#12463;&#12488;&#12398;&#29983;&#25104;
+      &#12288;<b>socket&#12458;&#12502;&#12472;&#12455;&#12463;&#12488;&#12398;&#29983;&#25104; </b>
     </p>
     <p>
-      &#12288;game&#12458;&#12502;&#12472;&#12455;&#12463;&#12488;&#12398;&#20316;&#25104;
+      <b>&#12288;game&#12458;&#12502;&#12472;&#12455;&#12463;&#12488;&#12398;&#20316;&#25104; </b>
     </p>
     <p>
-      &#12288;scene&#12398;&#20316;&#25104;
+      <b>&#12288;scene&#12398;&#20316;&#25104; </b>
     </p>
     <p>
-      &#12288;&#12288;titleScene
+      <b>&#12288;&#12288;titleScene </b>
     </p>
     <p>
-      &#12288;&#12288;playScene
+      <b>&#12288;&#12288;playScene </b>
     </p>
     <p>
-      &#12288;&#12288;practiceScene
+      <b>&#12288;&#12288;practiceScene </b>
     </p>
     <p>
-      &#12288;&#12288;resultScene
+      <b>&#12288;&#12288;resultScene</b>
     </p>
   </body>
 </html>
 </richcontent>
 </node>
 </node>
-<node CREATED="1340783995852" FOLDED="true" ID="ID_1117585894" MODIFIED="1340788019282" POSITION="right" TEXT="titleScene.js">
-<node CREATED="1340784006612" ID="ID_623796267" MODIFIED="1340784428410">
+<node CREATED="1340783995852" ID="ID_1117585894" MODIFIED="1341393731099" POSITION="right" TEXT="titleScene.js">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1340784006612" ID="ID_623796267" MODIFIED="1341393688695">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      init
+      <b>init </b>
     </p>
     <p>
-      &#12288;&#12525;&#12468;&#12398;&#34920;&#31034;
+      <b>&#12288;&#12525;&#12468;&#12398;&#34920;&#31034; </b>
     </p>
     <p>
-      &#12288;&#12508;&#12479;&#12531;&#12398;&#20316;&#25104;
+      <b>&#12288;&#12508;&#12479;&#12531;&#12398;&#20316;&#25104; </b>
     </p>
     <p>
-      &#12288;&#12288;practiceButton(&#32244;&#32722;)
+      <b>&#12288;&#12288;practiceButton(&#32244;&#32722;) </b>
     </p>
     <p>
-      &#12288;&#12288;playButton(&#23550;&#25126;)
+      <b>&#12288;&#12288;playButton(&#23550;&#25126;) </b>
     </p>
     <p>
-      &#12288;&#12288;rankingButton(&#12521;&#12531;&#12461;&#12531;&#12464;)
+      <b>&#12288;&#12288;rankingButton(&#12521;&#12531;&#12461;&#12531;&#12464;) </b>
     </p>
     <p>
-      &#12288;&#12288;questionButton(&#21839;&#38988;&#19968;&#35239;)
+      <b>&#12288;&#12288;questionButton(&#21839;&#38988;&#19968;&#35239;)</b>
     </p>
   </body>
-</html></richcontent>
+</html>
+</richcontent>
 </node>
-<node CREATED="1340784201559" ID="ID_395247489" MODIFIED="1340784625914">
+<node CREATED="1340784201559" ID="ID_395247489" MODIFIED="1341393693628">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      practiceButton
+      <b>practiceButton </b>
     </p>
     <p>
-      &#12288;&#12508;&#12479;&#12531;&#12452;&#12513;&#12540;&#12472;&#12398;&#25551;&#30011;
+      <b>&#12288;&#12508;&#12479;&#12531;&#12452;&#12513;&#12540;&#12472;&#12398;&#25551;&#30011; </b>
     </p>
     <p>
-      &#12288;clickEvent
+      <b>&#12288;clickEvent </b>
     </p>
     <p>
-      &#12288;&#12288;changeScene(&quot;practeceScene&quot;)
+      <b>&#12288;&#12288;changeScene(&quot;practeceScene&quot;)</b>
     </p>
   </body>
-</html></richcontent>
+</html>
+</richcontent>
 </node>
-<node CREATED="1340784201559" ID="ID_406889639" MODIFIED="1340784665781">
+<node CREATED="1340784201559" ID="ID_406889639" MODIFIED="1341393699707">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      playButton
+      <b>playButton </b>
     </p>
     <p>
-      &#12288;&#12508;&#12479;&#12531;&#12452;&#12513;&#12540;&#12472;&#12398;&#25551;&#30011;
+      <b>&#12288;&#12508;&#12479;&#12531;&#12452;&#12513;&#12540;&#12472;&#12398;&#25551;&#30011; </b>
     </p>
     <p>
-      &#12288;clickEvent
+      <b>&#12288;clickEvent </b>
     </p>
     <p>
-      &#12288;&#12288;changeScene(&quot;playScene&quot;)
+      <b>&#12288;&#12288;changeScene(&quot;playScene&quot;)</b>
     </p>
   </body>
-</html></richcontent>
+</html>
+</richcontent>
 </node>
-<node CREATED="1340784201559" ID="ID_1625899892" MODIFIED="1340784757965">
+<node CREATED="1340784201559" ID="ID_1625899892" MODIFIED="1341393709067">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      rankingButton
+      <b>rankingButton </b>
     </p>
     <p>
-      &#12288;&#12508;&#12479;&#12531;&#12452;&#12513;&#12540;&#12472;&#12398;&#25551;&#30011;
+      <b>&#12288;&#12508;&#12479;&#12531;&#12452;&#12513;&#12540;&#12472;&#12398;&#25551;&#30011; </b>
     </p>
     <p>
-      &#12288;clickEvent
+      <b>&#12288;clickEvent </b>
     </p>
     <p>
-      &#12288;&#12288;jump=&quot;/ranking&quot;
+      <b>&#12288;&#12288;jump=&quot;/ranking&quot;</b>
     </p>
   </body>
-</html></richcontent>
+</html>
+</richcontent>
 </node>
-<node CREATED="1340784201559" ID="ID_1310424234" MODIFIED="1340784765540">
+<node CREATED="1340784201559" ID="ID_1310424234" MODIFIED="1341393713289">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      questionButton
+      <b>questionButton </b>
     </p>
     <p>
-      &#12288;&#12508;&#12479;&#12531;&#12452;&#12513;&#12540;&#12472;&#12398;&#25551;&#30011;
+      <b>&#12288;&#12508;&#12479;&#12531;&#12452;&#12513;&#12540;&#12472;&#12398;&#25551;&#30011; </b>
     </p>
     <p>
-      &#12288;clickEvent
+      <b>&#12288;clickEvent </b>
     </p>
     <p>
-      &#12288;&#12288;jump=&quot;/question&quot;
+      <b>&#12288;&#12288;jump=&quot;/question&quot;</b>
     </p>
   </body>
-</html></richcontent>
+</html>
+</richcontent>
 </node>
 </node>
 <node CREATED="1340784779707" ID="ID_1337162496" MODIFIED="1340784792238" POSITION="right" TEXT="playScene.js">
-<node CREATED="1340784804920" ID="ID_8932072" MODIFIED="1340789325204">
+<node CREATED="1340784804920" ID="ID_8932072" MODIFIED="1341393744055">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      init
+      <b>init </b>
     </p>
     <p>
-      &#12288;&#12469;&#12540;&#12496;&#12395;&quot;getStandby&quot;&#12434;&#36865;&#12427;
+      <b>&#12288;&#12469;&#12540;&#12496;&#12395;&quot;getStandby&quot;&#12434;&#36865;&#12427; </b>
     </p>
     <p>
-      &#12288;socketcallback
+      <b>&#12288;socketcallback </b>
     </p>
     <p>
-      &#12288;&#12288;&quot;getRoomKey&quot;
+      <b>&#12288;&#12288;&quot;getRoomKey&quot; </b>
     </p>
     <p>
-      &#12288;&#12288;&#12288;createRoom(key)
+      <b>&#12288;&#12288;&#12288;createRoom(key)</b>
     </p>
   </body>
 </html>
 </richcontent>
 </node>
-<node CREATED="1340789333713" ID="ID_1776686780" MODIFIED="1340789571783">
+<node CREATED="1340789333713" ID="ID_1776686780" MODIFIED="1341393757443">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      createRoom(key)
+      <b>createRoom(key) </b>
     </p>
     <p>
-      &#12288;nameSpaceObject&#12398;&#29983;&#25104;
+      <b>&#12288;nameSpaceObject&#12398;&#29983;&#25104; </b>
     </p>
     <p>
-      &#12288;nameSpaceCallback
+      <b>&#12288;nameSpaceCallback </b>
     </p>
     <p>
-      &#12288;&#12288;&quot;connect&quot;
+      <b>&#12288;&#12288;&quot;connect&quot; </b>
     </p>
     <p>
-      &#12288;&#12288;&#12288;nameSpaceCallback
+      <b>&#12288;&#12288;&#12288;nameSpaceCallback </b>
     </p>
     <p>
-      &#12288;&#12288;&#12288;&#12288;/* Server&#12392;&#12398;&#12420;&#12426;&#12392;&#12426;&#12399;&#12371;&#12371;&#12363;&#12425;&#26360;&#12356;&#12390;&#12356;&#12367;*/
+      <b>&#12288;&#12288;&#12288;&#12288;&quot;gameStart&quot; =&gt; gameStart(scene, data)</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1341386297081" ID="ID_46594478" MODIFIED="1341393519747">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      gameStart(scene, data)
+    </p>
+    <p>
+      &#12288;scene.members = data[&quot;members&quot;];
+    </p>
+    <p>
+      &#12288;scene.questions = data[&quot;questions&quot;];
+    </p>
+    <p>
+      &#12288;&#12459;&#12454;&#12531;&#12488;&#12480;&#12454;&#12531;&#12398;&#38283;&#22987;(&#21839;&#38988;&#12398;&#24773;&#22577;&#12434;&#34920;&#31034;)
     </p>
   </body>
 </html>
